ca1b4a0a94251a7bb106a3fa00917f80fcf85d31
tthelen <tt@tobiasthelen.de>
2024-07-17 20:50:40 +0200

    Submit button reinserted, Profile message clearer

--- cogscinetwork/settings.py
+++ cogscinetwork/settings.py
@@ -124,8 +124,11 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    "/var/www/static/",
 ]
+
+# User uploaded files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
